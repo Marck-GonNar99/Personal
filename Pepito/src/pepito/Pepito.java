7225ac4067063e9640235079707883da2a6a1e8b
Marcos <mgmarcosdragon@gmail.com>
2021-01-19 11:36:01 +0100

    Ejercicio_1

---
+++ Pepito/src/pepito/Pepito.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepito;
+
+import java.util.Scanner;
+public class Pepito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sca=new Scanner(System.in); 
+        int num;
+        System.out.println("Pon un numero");
+        num=sca.nextInt();
+        System.out.println("Te la creiste WEy");
+    }
+    
+}
